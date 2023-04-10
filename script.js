42b57a5828cfc1e60c0b359463e8a3b1ddd57b08
Han Li <han.pecktree@gmail.com>
2023-04-10 16:01:21 -0600

    Add basic click function for number buttons and fix minor bugs

--- script.js
+++ script.js
@@ -24,7 +24,17 @@ function operate(a, b, operator) {
 
 function numButtonClicked(e) {
     num = e.target.id;
+    display.textContent += num;
 }
 
+function clearDisplay(e) {
+    clearButton.textContent = "0";
+}
+
+const display = document.querySelector(".display");
+
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", clearDisplay);
+
 const numButtons = document.querySelectorAll(".num-btn");
-numButtons.forEach(numButton => numButton.addEventListener(click, numButtonClicked));
+numButtons.forEach(numButton => numButton.addEventListener("click", numButtonClicked));
